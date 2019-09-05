bfb7851a55b57248d05a44effb4c215d28ba90df
ico1036 <jiwoong.kim@cern.ch>
2019-09-05 18:39:47 +0900

    delphes_compile

--- Delphes/compile.sh
+++ Delphes/compile.sh
@@ -1,3 +1,13 @@
-# --Edit the path 
-g++ `root-config --cflags` `root-config --libs` Ztuto.C -I/home/jkim/Generator/Delphes/Delphes-3.4.1 -L/home/jkim/Generator/Delphes/Delphes-3.4.1 -lDelphes -o Ztuto.exe
+####  Compiling Delphes ####
+##  --Please modify the path 
+
+### Specifying location of header file
+export PATH=/x3/cms/jelee/MCProd/Delphes3.4.2/classes:$PATH
+
+### Specifying location of lib.so files
+export LD_LIBRARY_PATH=/x3/cms/jelee/MCProd/Delphes3.4.2:$LD_LIBRARY_PATH
+
+### --Compile it!
+## -I(location of header) -L(location of lib.so) 
+g++ `root-config --cflags` `root-config --libs` $1.C -I/x3/cms/jelee/MCProd/Delphes3.4.2/classes -L/x3/cms/jelee/MCProd/Delphes3.4.2 -lDelphes -o $1.exe
 
